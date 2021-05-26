d7ddc7171373f12dec54688db8851055d579aff2
hwjung <hwjung@morai.ai>
2021-05-26 09:49:35 +0900

    first commit

--- vs_github_extension/Program.cs
+++ vs_github_extension/Program.cs
@@ -7,6 +7,8 @@ namespace vs_github_extension
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("This is develop branch");
         }
     }
 }
